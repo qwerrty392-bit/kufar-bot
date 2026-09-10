35392c144cc1427b6e31e5f9553cabb06c7d6a03
qwerrty392-bit <qwerrty392@gmail.com>
2026-09-10 14:00:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import telebot
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # --- КОНФИГУРАЦИЯ ---
-BOT_TOKEN = "8753909204:AAHH9FoRc3HF7e-R96OPqwpMIB8e2Hl7_M4"
+BOT_TOKEN = "8753909204:AAH1Fi8Fj4-cbdxfc34_xyR7nT2J2KUgxJk"
 
 SEARCH_QUERIES = [
     "205/55 R16",
