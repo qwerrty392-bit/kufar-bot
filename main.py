68c91bf414e9536b6017f2b68055414fc15c00e2
qwerrty392-bit <qwerrty392@gmail.com>
2026-09-10 14:22:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def save_data(filename, data):
 
 subscribers = set(load_data(USERS_FILE, []))
 seen_ads = set(load_data(SEEN_ADS_FILE, []))
-# save_data(SEEN_ADS_FILE, [])  # <-- РАСКОММЕНТИРУЙТЕ ОДИН РАЗ ДЛЯ ОЧИСТКИ БАЗЫ
+ save_data(SEEN_ADS_FILE, [])  # <-- РАСКОММЕНТИРУЙТЕ ОДИН РАЗ ДЛЯ ОЧИСТКИ БАЗЫ
 
 # --- 2. ПАРСИНГ KUFAR ---
 def fetch_kufar_ads(query):
